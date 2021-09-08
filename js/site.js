ed5676cea56931a2fa433c731222c4efd394abe8
Crom <m.daniel.omalley@gmail.com>
2021-09-08 19:06:51 -0400

    Init Commit
    Rewind Js challenge

--- js/site.js
+++ js/site.js
@@ -1,5 +1,32 @@
-//Call Hello world
+//Get the string value from the page
+function getValue(){
+    
+    document.getElementById("alert").classList.add("invisible");
 
-function helloWorld() {
-    alert("hello World");
+    let userString = document.getElementById("userString").value;
+
+    let revString = reverseString(userString);
+
+    displayString(revString);
+
+}
+
+// Reverse the string array
+// Logic function
+function reverseString(userString){
+
+    let revString = [];
+
+    
+    //reverse a string using a for loop
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+
+}
+//Display the reverse string array
+// View Function
+function displayString(){
+
 }
